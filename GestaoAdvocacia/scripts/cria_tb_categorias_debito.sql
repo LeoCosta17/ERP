60a3d9c932c62972ecdcdc8bce83f7be639d4eab
Leonardo Costa <leoocosta1209@gmail.com>
2026-06-26 08:03:48 -0300

    Separação projetos

--- GestaoAdvocacia/scripts/cria_tb_categorias_debito.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists tb_categorias_debito(
-    id BIGSERIAL PRIMARY KEY,
-    nome varchar(255) not null
-)
--- GestaoAdvocacia/scripts/cria_tb_dados_fiscais_empresa.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS tb_config_fiscais_empresa (
-    id BIGINT PRIMARY KEY ,
-    id_empresa BIGINT NOT NULL UNIQUE,
-    cd_regime_tributario INT NOT NULL,
-    FOREIGN KEY (id_empresa) REFERENCES tb_empresas(id)
-);
