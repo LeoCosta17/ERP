cafd81efeb61b42182a81de42a98a272364cbfa2
Leonardo Costa <leoocosta1209@gmail.com>
2026-07-01 13:16:40 -0300

    fix: corrigida a lógica de tratamento de resposta na função alterarSenhaAPI

--- Gestao/web/static/js/usuario/alterarSenha.js
+++ Gestao/web/static/js/usuario/alterarSenha.js
@@ -17,8 +17,13 @@ async function alterarSenhaAPI(senhaAtual, novaSenha, senhaConfirmacao) {
         })
     });
 
-    return await validaRespostaRequisicao(res);
+    // Se a resposta não for bem-sucedida, delegamos para a função que extrai a mensagem de erro do JSON.
+    if (!res.ok) {
+        return await validaRespostaRequisicao(res);
+    }
 
+    // Se a resposta for bem-sucedida (200 OK), o backend não retorna corpo.
+    // Não há nada para ser processado, então a função pode simplesmente terminar.
 }
 
 export async function alterarSenha(){
@@ -38,7 +43,7 @@ export async function alterarSenha(){
         const senhaConfirmacao = senhaConfirmacaoInput.value;
 
         try{
-            const res = await alterarSenhaAPI(senhaAtual, novaSenha, senhaConfirmacao);
+            await alterarSenhaAPI(senhaAtual, novaSenha, senhaConfirmacao);
             senhaAtualInput.value = '';
             novaSenhaInput.value = '';
             senhaConfirmacaoInput.value = '';
